4b95b2823fc1477d09d6dea2a5df68d4d4950869
EC2 Default User <ec2-user@ip-172-31-25-187.us-west-2.compute.internal>
2016-05-04 19:09:54 +0000

    removed jinq from webapi

--- KidosServer/controllers/webapi.js
+++ KidosServer/controllers/webapi.js
@@ -2,7 +2,6 @@ var mongoose=require('mongoose'),
 Schema=mongoose.Schema;
 var fs = require('fs');
 var mime=require('mime');
-var jinqJs = require('jinq');
 var cat = require('../models/categorymaster.js');
 var activities = require('../models/activities.js');
 
