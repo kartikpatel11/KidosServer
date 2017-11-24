3ed4428bcb33f2ff7490c8d7cfd93003d8ea0d19
Kartik <patel.kartikv@gmail.com>
2017-11-24 20:41:32 +0530

    kidosserver- category images

--- KidosServer/controllers/kidospartnersapi.js
+++ KidosServer/controllers/kidospartnersapi.js
@@ -102,7 +102,7 @@ exports.kidospartnerslogin=(function(req,res){
 //getactivitysummarybyuserid
 exports.getactivitysummarybyuserid=(function(req,res){
 	console.log("in getactivitysummarybyuserid-params: "+req.params.userid);
-	activities.find({ userid: req.params.userid },'activityId name area published contactdetails activitydetails locationdetails imagesdetails', function (err, docs) {
+	activities.find({ userid: req.params.userid },'activityId name area published contactdetails activitydetails locationdetails imagesdetails type.catImg', function (err, docs) {
 		res.json(200,docs);
 	});
 });
